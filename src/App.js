ecaa63d55a1cf089abccdb1f8f7aacca88f13ccd
Shah <shahbhumika70@gmail.com>
2021-02-21 00:23:47 +0530

    Login Layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Signup from './pages/signup/SignupPage';
+import Login from './pages/login/LoginPage';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Food Delivery App</h1>
+        <Login></Login>
         <Signup></Signup>
       </header>
     </div>
