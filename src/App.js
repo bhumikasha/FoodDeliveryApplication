00e043b2253b92e31b2c4954c94217fbdb86cd4a
Shah <shahbhumika70@gmail.com>
2021-02-20 17:38:49 +0530

    Signup Layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Signup from './pages/signup/SignupPage';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Food Delivery App</h1>
+        <Signup></Signup>
       </header>
     </div>
   );
